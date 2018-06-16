af43ef6a11d0a634122ac2e8c6759708d50f88d2
ThanhTung <>
2018-06-16 15:49:28 +0700

    commit dau tien

---
+++ MyApp/MyApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by ThanhTung on 5/22/18.
+//  Copyright © 2018 thanh tung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setu ƒp after loading the view, typically from a nib.
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
